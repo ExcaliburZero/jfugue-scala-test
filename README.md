8a169a6efee518ed79d6e7faaf970baa358db4b7
Christopher Wells <cwellsny@nycap.rr.com>
2017-11-28 10:44:07 -0500

    Add more usage info

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ A test of using JFugue, a jvm music library, in Scala.
 
 ## Setup
 ```
+$ sudo apt install fluidsynth
 $ mkdir lib
 $ curl http://www.jfugue.org/jfugue-5.0.9.jar > lib/jfugue-5.0.9.jar
+```
+
+## Usage
+```
+$ make server &
+$ sbt assembly
+$ java -jar target/scala-2.12/jfuguetest-assembly-1.0.jar
 ```
--- src/main/scala/jfuguetest/Main.scala
+++ src/main/scala/jfuguetest/Main.scala
@@ -1,7 +1,22 @@
 package jfuguetest
 
+import org.jfugue.realtime.RealtimePlayer
+import org.jfugue.theory.Note
+import javax.sound.midi.MidiSystem
+import javax.sound.midi.MidiDevice
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    val devices: Array[MidiDevice.Info] = MidiSystem.getMidiDeviceInfo
+    if (devices.length == 0) {
+      println("No MIDI devices found")
+    } else {
+      for (dev <- devices) {
+        println(dev)
+      }
+    }
+
+    val player = new RealtimePlayer()
+    player.play("C D E F G A B");
   }
 }
